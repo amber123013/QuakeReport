28e58a305b4d8f39c90c9eb0aea15078bb839116
amber <1193756987@qq.com>
2017-02-17 21:42:52 +0800

    1.新建一个线程，访问网站并返回JSON数据，解析后放入ListView中 2.此版本存在bug  -- 当运行软件后，改变屏幕方向，系统会新建一个Activity并执行请求线程 而前一个并不会被回收(因为EarthquakeAsyncTask是内部类)

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,6 +17,7 @@ package com.example.android.quakereport;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -24,35 +25,37 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
+
+    private EarthquaAdapter mAdapter;
+
+    private static final String USGS_REQUEST_URL =
+            "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=5&limit=30";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-        final ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
-
-        // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
-        // Create a new {@link ArrayAdapter} of earthquakes
-        final EarthquaAdapter adapter = new EarthquaAdapter(this , earthquakes);
+        // 创建新适配器，将空地震列表作为输入
+        mAdapter = new EarthquaAdapter(this, new ArrayList<Earthquake>());
+        // 在 {@link ListView} 上设置适配器
+        // 以便可以在用户界面中填充列表
+        earthquakeListView.setAdapter(mAdapter);
 
-        // Set the adapter on the {@link ListView}
-        // so the list can be populated in the user interface
-        earthquakeListView.setAdapter(adapter);
-
-        //设置列表项监听
+        // 在 ListView 上设置项目单击监听器，该监听器会向 Web 浏览器发送 intent，
+        // 打开包含有关所选地震详细信息的网站。
         earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 //点击的 当前地震
-                Earthquake currentEarthquake = earthquakes.get(position);
+                Earthquake currentEarthquake = mAdapter.getItem(position);
                 // 将字符串 URL 转换为 URI 对象（以传递至 Intent 中 constructor)
                 Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
                 //创建查看详细信息的intent
@@ -61,5 +64,35 @@ public class EarthquakeActivity extends AppCompatActivity {
                 startActivity(webIntent);
             }
         });
+        // 启动 AsyncTask 以获取地震数据
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(USGS_REQUEST_URL);
+
+    }
+
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<Earthquake>> {
+
+        @Override
+        protected List<Earthquake> doInBackground(String... params) {
+            // 如果不存在任何 URL 或第一个 URL 为空，不执行执行。
+            if (params.length < 1 || params[0] == null) {
+                return null;
+            }
+
+            List<Earthquake> result = QueryUtils.fetchEarthquakeData(params[0]);
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(List<Earthquake> earthquakes) {
+            // 清除之前地震数据的适配器
+            mAdapter.clear();
+
+            // 如果存在 {@link Earthquake} 的有效列表，则将其添加到适配器的
+            // 数据集。这将触发 ListView 执行更新。
+            if (earthquakes != null && !earthquakes.isEmpty()) {
+                mAdapter.addAll(earthquakes);
+            }
+        }
     }
 }
