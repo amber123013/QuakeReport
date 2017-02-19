87fd475df9a7e9c13484f5db214f68f20f1219d0
amber <1193756987@qq.com>
2017-02-19 14:20:11 +0800

    在EarthquakeActivity添加Menu 点击Menu intent跳转到SettingsActivity SettingsActivity 使用PreferenceFragment作为view

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -25,6 +25,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -155,7 +157,24 @@ public class EarthquakeActivity extends AppCompatActivity implements android.app
     public void onLoaderReset(Loader<List<Earthquake>> loader) {
        // Log.v(LOG_TAG,"重置了Loader");
         // 重置 Loader，以便能够清除现有数据。
-       // 、、mAdapter.clear();
+       mAdapter.clear();
+    }
+    //添加Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            //选中的是action_settings
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
